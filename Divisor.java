40369768bfe3718047ccd56ab066dbd34d850528
Ajaikumaran <121105946+Ajaikumaran@users.noreply.github.com>
2022-12-29 13:49:57 +0530

    Add files via upload

---
+++ Divisor.java
@@ -0,0 +1,18 @@
+package program;
+public class Divisor {
+public static void main(String[] args) {
+int number=1000,i=1,count=1;
+for(;i<=number/2;i++) {
+if(number%i==0) {
+	System.out.print(i+" ");
+	count++;
+
+}
+}
+System.out.println(number);
+System.out.println();
+System.out.println("No of Divisors are : "+count);
+
+	}
+
+}
