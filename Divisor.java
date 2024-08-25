0b1917191335e4956cfb0b1b9bbb931896066adb
Ajaikumaran <ajaikumaran1@gmail.com>
2024-08-25 20:58:11 +0530

    sample

--- Divisor.java
+++ Divisor.java
@@ -1,17 +1,17 @@
-package program;
+//package program;
 public class Divisor {
-public static void main(String[] args) {
-int number=1000,i=1,count=1;
-for(;i<=number/2;i++) {
-if(number%i==0) {
-	System.out.print(i+" ");
-	count++;
+	public static void main(String[] args) {
+		int number = 1000, i = 1, count = 1;
+		for (; i <= number / 2; i++) {
+			if (number % i == 0) {
+				System.out.print(i + " ");
+				count++;
 
-}
-}
-System.out.println(number);
-System.out.println();
-System.out.println("No of Divisors are : "+count);
+			}
+		}
+		System.out.println(number);
+		System.out.println();
+		System.out.println("No of Divisors are : " + count);
 
 	}
 
